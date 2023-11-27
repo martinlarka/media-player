84646578fcf1c2e91a4d89939bedd17dfd0ac782
Martin Lärka <martin.larka@gmail.com>
2023-11-27 13:39:59 +0100

    Handle Playlist events

--- src/Playlist/Playlist.tsx
+++ src/Playlist/Playlist.tsx
@@ -1,9 +1,31 @@
 import { twMerge } from "tailwind-merge";
 import { usePlaylistState } from "../Context/PlaylistContext";
+import { usePlayerState } from "../Context/PlayerContext";
 
 export const Playlist = () => {
   const [playlist, setPlaylist] = usePlaylistState();
-  console.log("playlist", playlist);
+  const [playerState, setPlayer] = usePlayerState();
+
+  const onAddMedia = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const mediaUrl = e.currentTarget.elements[0] as HTMLInputElement;
+    const newUrl = mediaUrl.value;
+    setPlaylist((list) => [...list, newUrl]);
+    mediaUrl.value = "";
+  };
+
+  const onRemoveMedia = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    i: number
+  ) => {
+    e.stopPropagation();
+    setPlaylist((list) => list.slice(0, i).concat(list.slice(i + 1)));
+  };
+
+  const onSelectMedia = (mediaUrl: string) => {
+    setPlayer({ playingUrl: mediaUrl });
+  };
+
   return (
     <div className="flex flex-col justify-between">
       <div>
@@ -16,35 +38,20 @@ export const Playlist = () => {
               "px-2 py-2",
               "border-b",
               "group",
-              "cursor-pointer"
+              "cursor-pointer",
+              playerState.playingUrl === item && "font-black"
             )}
+            onClick={() => onSelectMedia(item)}
           >
             <span className="group-hover:underline">
               {item.match(/(?=\w+\.\w{3,4}$).+/) || item}
             </span>
-            <button
-              onClick={(e) => {
-                e.stopPropagation();
-                setPlaylist((list) =>
-                  list.slice(0, i).concat(list.slice(i + 1))
-                );
-              }}
-            >
-              Remove
-            </button>
+            <button onClick={(e) => onRemoveMedia(e, i)}>Remove</button>
           </div>
         ))}
       </div>
       <div>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            const mediaUrl = e.currentTarget.elements[0] as HTMLInputElement;
-            const newUrl = mediaUrl.value;
-            setPlaylist((list) => [...list, newUrl]);
-            mediaUrl.value = "";
-          }}
-        >
+        <form onSubmit={(e) => onAddMedia(e)}>
           <label htmlFor="media-url">Media url:</label>
           <br />
           <input type="text" id="media-url" name="mediaUrl" required />
